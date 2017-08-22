96118c4507f70b100ff6e8c4833a7413c8fb55a3
Karina Kozarova <karinakozarova1303@gmail.com>
2017-08-22 19:06:47 +0300

    It works!

--- simon.rb
+++ simon.rb
@@ -1,6 +1,6 @@
 def board
   # creates initial board
-  print "\n1 2\n3 4\n"
+  print "\n0 1\n2 3\n"
   "OO\nOO"
 end
 
@@ -17,6 +17,7 @@ def rand_position
   num = rand(0..3) # generates the position of the number
   arr = ["O","O","O","O"] # default array
   $turns << num
+  $all << arr
   arr[num] = "X" # symbol we print when there is a change
   arr[0]+arr[1] + "\n" + arr[2] + arr[3] # returns formatted array
 end
@@ -40,9 +41,11 @@ moves = 0
 $turns = Array.new # creates new array
 while 1
   puts board
-  $all.each{|x| puts x[0] + x[1] + "\n" + x[2] + x[3]}  
+  $all.each{|x|
+    sleep(2)
+    puts x[0] + x[1] + "\n" + x[2] + x[3]
+  }
   increment_moves # get new move
-  sleep(2)
   print("Input?")
 
   flag = 1
@@ -56,7 +59,7 @@ while 1
   end
 
   winning(flag)
-  if flag==0 # end infinite loop
+  if flag == 0 # end infinite loop
     break
   end
 end
