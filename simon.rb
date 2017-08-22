c4cf286e0e9622f67429c2339a51025246b34d75
Karina Kozarova <karinakozarova1303@gmail.com>
2017-08-22 18:49:40 +0300

    refactored

--- simon.rb
+++ simon.rb
@@ -14,9 +14,9 @@ end
 def rand_position
   num = rand(0..3) # generates the position of the number
   arr = ["O","O","O","O"] # default array
-  $turns << num 
+  $turns << num
   arr[num] = "X" # symbol we print when there is a change
-  arr[0]+arr[1] + "\n" + arr[2] + arr[3] # returns formatted array 
+  arr[0]+arr[1] + "\n" + arr[2] + arr[3] # returns formatted array
 end
 
 def winning(flag)
@@ -29,7 +29,7 @@ def winning(flag)
 end
 
 def increment_moves
-  # gets new random position 
+  # gets new random position
   sleep(2) # waits 2 seconds
   puts rand_position
 end
@@ -38,7 +38,8 @@ moves = 0
 
 while 1
   $turns = Array.new # creates new array
-  puts board 
+  puts board
+  flag = 1
   for i in 0..moves
     increment_moves # get new move
   end
@@ -46,10 +47,7 @@ while 1
   sleep(2)
   print("Input?")
 
-  flag = 1
-  l = $turns.length # length of the array turns
-  puts l
-  for i in 0...l
+  for i in 0...$turns.length 
     input_turns = gets.chomp.to_i
     puts x = $turns[i]
     if input_turns != x # checks if entered move is different from output
@@ -60,6 +58,6 @@ while 1
 
   winning(flag)
   if flag==0 # end infinite loop
-    break 
+    break
   end
 end
